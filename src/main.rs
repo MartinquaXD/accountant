2ae60a469b59aa14e4f51342d6e50edef3b748ec
Martin Beckmann <martin@froglogic.com>
2021-06-21 16:44:28 +0200

    Implement main
    Now everything gets put together.
    -Read first CLI argument as file name
    -Read csv data from that file
    -Create a Transaction from each row
    -Execute each Transaction
    -Print out the final state of the AccountManager

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,22 @@ mod account_manager;
 mod helper_types;
 mod transaction;
 
-fn main() {
-    println!("Hello, world!");
+use crate::account_manager::AccountManager;
+use crate::transaction::Transaction;
+use std::convert::TryFrom;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = std::env::args().collect();
+    let input_file = args.get(1).ok_or("no input file argument passed")?;
+
+    let mut account_manager = AccountManager::default();
+
+    let mut reader = csv::Reader::from_path(input_file)?;
+    for read_result in reader.records() {
+        if let Ok(transaction) = Transaction::try_from(read_result?) {
+            account_manager.handle_transaction(transaction);
+        }
+    }
+    println!("{}", account_manager);
+    Ok(())
 }
