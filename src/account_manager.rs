57cd15076ffe6431336b62cd4853317a0b82d98c
Martin Beckmann <martin@froglogic.com>
2021-06-21 16:37:28 +0200

    Execute Transactions in AccountManager
    Here we implement the real funcitonality.
    -Withdrawals only get executed when if the account holds enough money
    -Only executed Withdrawals can be disputed
    -Only executed Disputes can get resolved or charged back
    -Transactions only get executed for unlocked accounts
    -Accounts get created during the first Deposit

--- src/account_manager.rs
+++ src/account_manager.rs
@@ -1,6 +1,7 @@
 use crate::helper_types::{TransactionId, UserId};
-use crate::transaction::Transaction;
+use crate::transaction::{Amount, Transaction};
 use std::collections::HashMap;
+use Transaction::{Chargeback, Deposit, Dispute, Resolve, Withdrawal};
 
 #[derive(Copy, Clone, Debug, Default)]
 struct Account {
@@ -48,9 +49,98 @@ impl TransactionRecord {
     }
 }
 
-
 #[derive(Clone, Debug, Default)]
 pub struct AccountManager {
     accounts: HashMap<UserId, Account>,
     transactions: HashMap<TransactionId, TransactionRecord>,
+}
+
+impl AccountManager {
+    pub fn handle_transaction(&mut self, transaction: Transaction) -> Option<()> {
+        match transaction {
+            Deposit { user, tx, amount } => self.handle_deposit(user, tx, amount),
+            Withdrawal {
+                user,
+                tx: _,
+                amount,
+            } => self.handle_withdrawal(user, amount),
+            Dispute { user, tx } => self.handle_dispute(user, tx),
+            Resolve { user, tx } => self.handle_resolve(user, tx),
+            Chargeback { user, tx } => self.handle_chargeback(user, tx),
+        };
+        Some(())
+    }
+
+    fn handle_deposit(&mut self, user: UserId, tx: TransactionId, amount: Amount) {
+        //I guess only deposits should create accounts
+        let account = self
+            .accounts
+            .entry(user)
+            .or_insert_with(|| Account::default());
+        account.available += amount.0 as i128;
+        account.total += amount.0 as i128;
+        self.transactions
+            .insert(tx, TransactionRecord::new(Deposit { user, tx, amount }));
+    }
+
+    fn handle_withdrawal(&mut self, user: UserId, amount: Amount) {
+        if let Some(account @ Account { locked: false, .. }) = self.accounts.get_mut(&user) {
+            if account.available >= amount.0 as i128 {
+                account.available -= amount.0 as i128;
+                account.total -= amount.0 as i128;
+            }
+        }
+    }
+
+    fn handle_dispute(&mut self, user: UserId, tx: TransactionId) {
+        use DisputeState::Initial;
+        if let Some(account @ Account { locked: false, .. }) = self.accounts.get_mut(&user) {
+            self.transactions.entry(tx).and_modify(|entry| {
+                if let TransactionRecord {
+                    transaction: Deposit { amount, .. },
+                    state: Initial,
+                } = entry
+                {
+                    account.available -= amount.0 as i128;
+                    account.held += amount.0 as i128;
+                    entry.dispute();
+                }
+            });
+        }
+    }
+
+    fn handle_resolve(&mut self, user: UserId, tx: TransactionId) {
+        use DisputeState::Disputed;
+        if let Some(account @ Account { locked: false, .. }) = self.accounts.get_mut(&user) {
+            self.transactions.entry(tx).and_modify(|entry| {
+                if let TransactionRecord {
+                    transaction: Deposit { amount, .. },
+                    state: Disputed,
+                } = entry
+                {
+                    account.available -= amount.0 as i128;
+                    account.held += amount.0 as i128;
+                    entry.resolve();
+                }
+            });
+        }
+    }
+
+    fn handle_chargeback(&mut self, user: UserId, tx: TransactionId) {
+        use DisputeState::Disputed;
+        if let Some(account @ Account { locked: false, .. }) = self.accounts.get_mut(&user) {
+            self.transactions.entry(tx).and_modify(|entry| {
+                if let TransactionRecord {
+                    transaction: Deposit { amount, .. },
+                    state: Disputed,
+                } = entry
+                {
+                    account.held -= amount.0 as i128;
+                    account.total -= amount.0 as i128;
+                    account.locked = true;
+                    entry.chargeback();
+                }
+            });
+        }
+    }
 }
